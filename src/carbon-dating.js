7d6667d7e402a961cd4b6b240229f1967b63420a
RomaaaK <zimpsonzya@gmail.com>
2022-05-01 20:49:08 +0300

    feat: implement other tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,10 +19,19 @@ const HALF_LIFE_PERIOD = 5730;
  */
 
 function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (isNaN(sampleActivity) || typeof sampleActivity !== 'string' || sampleActivity > 15 || sampleActivity < 1) 
+  {
+    return false
+  }
+
+  const ages =  0.693 / HALF_LIFE_PERIOD
+  const speed = Math.log(MODERN_ACTIVITY / sampleActivity)
+
+  return Math.ceil(speed / ages)
 }
 
 module.exports = {
   dateSample
 };
+
+
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let counter = 0
+    let result = ""
+
+    for (let i = 0; i < str.length; i++) {
+      let l1 = 0
+      let l2 = 0
+      if (str[i].toUpperCase().charCodeAt(0) > 64 && str[i].toUpperCase().charCodeAt(0) < 91) { 
+        counter = counter > (key.length - 1) ? 0 : counter
+        l1 = this.alphabet.indexOf(str[i].toUpperCase())
+        l2 = this.alphabet.indexOf(key[counter++].toUpperCase())
+        result += this.alphabet[(l1 + l2) > 25 ? (l1 + l2) - 26 : (l1 + l2)]
+        continue
+      }
+      result += str[i]
+    }
+
+    if (!this.reverse) {
+      return result.split('').reverse().join('')
+    }
+
+    return result
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+    let result = ''
+    let counter = 0
+    
+    for (let i = 0; i < str.length; i++) {
+      if (str[i].toUpperCase().charCodeAt(0) > 64 && str[i].toUpperCase().charCodeAt(0) < 91) { 
+        counter = counter > (key.length - 1) ? 0 : counter
+        result += this.alphabet.slice(str[i].charCodeAt(0) - key[counter++].toUpperCase().charCodeAt(0))[0]
+        continue
+      }
+        result += str[i]
+    }
+
+    if (!this.reverse) {
+      return result.split('').reverse().join('')
+    }
+
+    return result
   }
 }
 
